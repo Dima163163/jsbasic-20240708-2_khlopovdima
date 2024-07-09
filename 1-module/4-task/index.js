40a83b44c89ede99101c3882b9cce83c9e1c30d0
Dima163163 <khlopovdima@gmail.com>
2024-07-09 10:08:06 +0400

    lesson-1 task: 2, 3, 4, 5

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,9 @@
 function checkSpam(str) {
-  // ваш код...
+  const spamStr = str.toLowerCase();
+
+  if (spamStr.includes('1xbet') || spamStr.includes('xxx')) {
+    return true;
+  }
+
+  return false;
 }
