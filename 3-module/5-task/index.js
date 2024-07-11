6cdf9a3f8f3b8fc723fb0187b844ba48377491e8
Dima163163 <khlopovdima@gmail.com>
2024-07-11 09:30:41 +0400

    lesson3 task: 4, 5

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,8 @@
 function getMinMax(str) {
-  // ваш код...
+  const numbersArr = str.split(' ').filter(item => !isNaN(+item));
+
+  return {
+    min: Math.min(...numbersArr),
+    max: Math.max(...numbersArr),
+  };
 }
