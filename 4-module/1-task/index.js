60b14f5dc5f20aa8db2d906c00742a371f3fecae
Dima163163 <khlopovdima@gmail.com>
2024-07-12 10:41:25 +0400

    lesson-4 task: 1

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,11 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  const ul = document.createElement('ul');
+
+  for (let friend of friends) {
+    const li = document.createElement('li');
+    li.innerText = `${friend.firstName} ${friend.lastName}`;
+    ul.append(li);
+  }
+  
+  return ul;
 }
