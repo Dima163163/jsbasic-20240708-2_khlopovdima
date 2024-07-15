24b9ca214a7a2cbcc2895767b9801b47fe5cba66
Dima163163 <khlopovdima@gmail.com>
2024-07-15 14:42:16 +0400

    lesson-5 task-1-fix fask-2 completed

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -2,10 +2,6 @@ function hideSelf() {
   const btn = document.querySelector('.hide-self-button');
 
   btn.addEventListener('click', () => {
-    if (!btn.hidden) {
-      btn.hidden = true;
-    } else {
-      btn.hidden = false;
-    }
+    btn.hidden = !btn.hidden;
   });
 }
