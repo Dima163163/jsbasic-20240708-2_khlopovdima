41defa1eacfaf1dca16a64ddd8e064447add7c03
Dima163163 <khlopovdima@gmail.com>
2024-07-18 12:08:03 +0400

    lesson-6 task-1 fixed, task-2 completed

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,3 +1,4 @@
+import createElement from '../../assets/lib/create-element.js';
 /**
  * Компонент, который реализует таблицу
  * с возможностью удаления строк
@@ -14,62 +15,42 @@
  */
 export default class UserTable {
   constructor(rows) {
-    this.elem = this.createTable(rows);
-    this.checkDelete(this.elem);
+    this.renderTable(rows);
+    this.checkDelete();
   }
 
-  createTable(rows) {
-    const table = document.createElement('table');
-    const thead = document.createElement('thead');
-    const trHead = document.createElement('tr');
-
-    const thName = document.createElement('th');
-    thName.textContent = 'Имя';
-    const thAge = document.createElement('th');
-    thAge.textContent = 'Возраст';
-    const thSalary = document.createElement('th');
-    thSalary.textContent = 'Зарплата';
-    const thCity = document.createElement('th');
-    thCity.textContent = 'Город';
-    const thDelete = document.createElement('th');
-
+  renderTable(rows) {
+    this.elem = createElement(`
+      <table>
+        <thead>
+          <tr>
+              <th>Имя</th>
+              <th>Возраст</th>
+              <th>Зарплата</th>
+              <th>Город</th>
+              <th></th>
+          </tr>
+        </thead>
+      </table>
+    `);
     const tBody = document.createElement('tbody');
 
-    trHead.append(thName, thAge, thSalary, thCity, thDelete);
-    thead.append(trHead);
-    table.append(thead, tBody);
-
-
-    rows.forEach(item => {
+    rows.forEach((item) => {
       const trBody = document.createElement('tr');
-
-      const tdName = document.createElement('td');
-      tdName.textContent = item.name;
-
-      const tdAge = document.createElement('td');
-      tdAge.textContent = item.age;
-
-      const tdSalary = document.createElement('td');
-      tdSalary.textContent = item.salary;
-
-      const tdCity = document.createElement('td');
-      tdCity.textContent = item.city;
-
-      const tdDelete = document.createElement('td');
-
-      const deleteButton = document.createElement('button');
-      deleteButton.textContent = 'X';
-
-      tdDelete.append(deleteButton);
-      trBody.append(tdName, tdAge, tdSalary, tdCity, tdDelete);
+      trBody.innerHTML = `
+      <td>${item.name}</td>
+      <td>${item.age}</td>
+      <td>${item.salary}</td>
+      <td>${item.city}</td>
+      <td><button>X</button></td>
+      `;
       tBody.append(trBody);
     });
-
-    return table;
+    this.elem.append(tBody);
   }
 
-  checkDelete(elem) {
-    elem.addEventListener('click', (e) => {
+  checkDelete() {
+    this.elem.addEventListener('click', (e) => {
       const target = e.target;
       if (target.matches('button')) {
         const elementTr = target.closest('tr');
