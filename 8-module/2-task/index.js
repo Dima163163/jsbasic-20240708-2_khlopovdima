0bfe999e95b12723758847ffd7cb9a93c3a4b776
Dima163163 <khlopovdima@gmail.com>
2024-07-30 13:55:40 +0400

    lesson-8 task-2

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -5,5 +5,46 @@ export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
+    this.renderProductGrid();
+  }
+
+  renderProductGrid() {
+    this.elem = createElement(`
+      <div class="products-grid">
+        <div class="products-grid__inner">
+        </div>
+      </div>
+    `);
+
+    this.products.forEach(product => {
+      const card = new ProductCard(product);
+      this.elem.querySelector('.products-grid__inner').append(card.elem);
+    });
+  }
+
+  updateFilter(filters) {
+    Object.assign(this.filters, filters);
+
+    this.products = this.products.filter((product) => {
+      if (this.filters.noNuts) {
+        return product.nuts !== this.filters.noNuts;
+      }
+      
+      if (this.filters.vegeterianOnly) {
+        return product.vegeterian === this.filters.vegeterianOnly;
+      }
+
+      if (this.filters.category) {
+        return product.category === this.filters.category;
+      }
+
+      if (this.filters.maxSpiciness) {
+        return product.spiciness <= this.filters.maxSpiciness;
+      }
+
+      return product;
+    });
+
+    this.renderProductGrid();
   }
 }
